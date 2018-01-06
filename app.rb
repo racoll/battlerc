b9139e8118631ca822d8cd47dc1b2e105dd3e8ba
robincollins <racollins1@me.com>
2018-01-06 18:19:08 +0000

    added params name to get name from client request

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require "sinatra"
 
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+  erb :index
+end
+
+get "/named-cat" do
+  p params
+  @name = params[:name]
+  erb :index
 end
