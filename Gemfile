1e14d65b6d693d33aa0a6c2f4fd189c050381090
robincollins <racollins1@me.com>
2018-01-06 17:26:54 +0000

    added html content to get request

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source "https://rubygems.org"
 
   gem "sinatra"
+  gem "shotgun"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
--- app.rb
+++ app.rb
@@ -7,3 +7,13 @@ end
 get "/secret" do
   "Hi Robin"
 end
+
+get "/lily" do
+  "Hi Lily"
+end
+
+get "/cat" do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
