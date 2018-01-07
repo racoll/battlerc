683a59d8a22acba4356ac0d32c9d44d483fb3e32
robincollins <racollins1@me.com>
2018-01-07 19:20:37 +0000

    set up battle and capybara

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ source "https://rubygems.org"
   gem "shotgun"
   gem "capybara"
   gem "selenium-webdriver"
+  gem "rspec"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
--- app.rb
+++ app.rb
@@ -1,17 +1,10 @@
 require "sinatra"
 
+class Battle < Sinatra::Base
+  get '/' do
+    'Hello Battle!'
+  end
 
-get "/random-cat" do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb :index
-end
-
-post "/named-cat" do
-  p params
-  @name = params[:name]
-  erb :index
-end
-
-get '/cat-form' do
-  erb :cat_form
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
