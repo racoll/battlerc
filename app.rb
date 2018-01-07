6e0d90672572b67f46e06c3441437aca42c1a8d0
robincollins <racollins1@me.com>
2018-01-07 20:01:32 +0000

    corrected error now working post redirect play view

--- app.rb
+++ app.rb
@@ -14,17 +14,20 @@ enable :sessions
   #   erb :play
   # end
 
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect "/play"
+  end
+
+
   get "/play" do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
     erb :play
   end
 
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    erb :play
-  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
