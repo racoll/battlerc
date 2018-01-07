260cd72be99af3588adc3cca9f9b3ef4941f0130
robincollins <racollins1@me.com>
2018-01-07 19:42:42 +0000

    created index and play views

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require "sinatra"
 
 class Battle < Sinatra::Base
-  
+
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+  end
+
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
