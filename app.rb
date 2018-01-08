065c28e58dd77d5e12e2c18a5411706938b7850f
robincollins <racollins1@me.com>
2018-01-08 20:09:52 +0000

    used global variable temporarily to reduce use of sessions

--- app.rb
+++ app.rb
@@ -7,30 +7,41 @@ enable :sessions
     erb :index
   end
 
-
   # post '/names' do
-  #   @player_1_name = params[:player_1_name]
-  #   @player_2_name = params[:player_2_name]
+  #   session[:player_1_name] = params[:player_1_name]
+  #   session[:player_2_name] = params[:player_2_name]
+  #   redirect "/play"
+  # end
+
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
+  end
+
+
+  # get "/play" do
+  #   @player_1_name = session[:player_1_name]
+  #   @player_2_name = session[:player_2_name]
   #   erb :play
   # end
 
-
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    redirect "/play"
-  end
-
-
-  get "/play" do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+  get '/play' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
+
+  # get "/attack" do
+  #   @player_1_name = session[:player_1_name]
+  #   @player_2_name = session[:player_2_name]
+  #   erb :attack
+  # end
+
   get "/attack" do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
